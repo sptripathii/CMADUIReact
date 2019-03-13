26ebd4ea396b1fc976b3c2ba2b357bf7be4c5a26
sudhatri <sudhatri@cisco.com>
2019-03-14 01:34:27 +0530

    Added pom.xml related changes

---
+++ target/nmslogui/precache-manifest.c45fd2f75532e96b81c9a41dcea1abc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d00b45effaec4296b925000057675c3",
+    "url": "http://localhost:8080/nmslogui/static/media/logo.7d00b45e.png"
+  },
+  {
+    "revision": "1952fdebbec1de2ea723",
+    "url": "http://localhost:8080/nmslogui/static/js/runtime~main.1952fdeb.js"
+  },
+  {
+    "revision": "0dd6a28465b03bee8ea8",
+    "url": "http://localhost:8080/nmslogui/static/js/main.0dd6a284.chunk.js"
+  },
+  {
+    "revision": "494ef090433d53555834",
+    "url": "http://localhost:8080/nmslogui/static/js/1.494ef090.chunk.js"
+  },
+  {
+    "revision": "0dd6a28465b03bee8ea8",
+    "url": "http://localhost:8080/nmslogui/static/css/main.2d9e15ea.chunk.css"
+  },
+  {
+    "revision": "773adf956e7e1c5e4ade849647a07554",
+    "url": "http://localhost:8080/nmslogui/index.html"
+  }
+];
---
+++ target/nmslogui/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "http://localhost:8080/nmslogui/precache-manifest.c45fd2f75532e96b81c9a41dcea1abc4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("http://localhost:8080/nmslogui/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
